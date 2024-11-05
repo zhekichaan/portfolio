f504c2ff415b8dfa846ad1f1ad1adc2284e66575
zhekichaan <jeck.chubchik@gmail.com>
2024-11-04 21:23:59 -0500

    done sections home, projects and contact. added icons and resume

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const sections = document.querySelectorAll("section");
 const navButtons = document.querySelectorAll(".nav-item");
 
 const handleSectionChange = (sec, btn) => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
   navButtons.forEach((btn) => {
     btn.classList.remove("selected");
   });
@@ -15,3 +18,32 @@ const handleSectionChange = (sec, btn) => {
     }
   });
 };
+
+const form = document.querySelector("#contactForm");
+
+// Event listener that displays a successful message when the form is submitted
+form.addEventListener(
+  "submit",
+  (handleSubmit = (e) => {
+    e.preventDefault();
+    form.reset();
+    Swal.fire({
+      title: "Done!",
+      text: "Message sent successfuly!",
+      icon: "OK",
+      color: "#fff",
+      background: "#000",
+      customClass: {
+        container: "...",
+        popup: "popup-container",
+        header: "...",
+        title: "...",
+        closeButton: "...",
+        icon: "...",
+        image: "...",
+        htmlContainer: "...",
+        confirmButton: "button",
+      },
+    });
+  })
+);
