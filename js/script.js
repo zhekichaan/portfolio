2381ccbbb3173d22136393bd6049293da8712ac9
zhekichaan <y.chernytskyi@gmail.com>
2024-11-06 16:41:55 -0500

    fixed section title and closing of menu

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ const handleMenuOpen = () => {
   document.querySelector(".nav").classList.toggle("open");
 };
 
+const handleMenuClose = () => {
+  document.querySelector(".nav").classList.remove("open");
+};
+
 const handleSectionChange = (sec, btn) => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+  handleMenuClose();
 
   navButtons.forEach((btn) => {
     btn.classList.remove("selected");
